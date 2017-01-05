9f8efca83dfab298c6c1fe29aa577006d040e84d
DanielGomez2016 <daniel.gomez@chihuahua.gob.mx>
2017-01-05 11:18:57 -0700

    primer commit

---
+++ MemotraficoV2/MemotraficoV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MemotraficoV2.MvcApplication" Language="C#" %>
---
+++ MemotraficoV2/MemotraficoV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MemotraficoV2.Startup))]
+namespace MemotraficoV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
