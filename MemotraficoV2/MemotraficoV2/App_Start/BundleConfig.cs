4e22cd88c9f6647d7351ccbd5e4438f9007b7e67
DanielGomez2016 <daniel.gomez@chihuahua.gob.mx>
2017-02-14 20:56:45 -0700

    cambios

--- MemotraficoV2/MemotraficoV2/App_Start/BundleConfig.cs
+++ MemotraficoV2/MemotraficoV2/App_Start/BundleConfig.cs
@@ -9,10 +9,7 @@ namespace IdentitySample
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-{version}.min.js",
-                        "~/Scripts/pace.js",
-                        "~/Scripts/main.js",
-                        "~/Scripts/head-1.0.3.js"));
+                        "~/Scripts/jquery-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,6 +19,35 @@ namespace IdentitySample
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                
+                        "~/Content/Proyecto/jquery/dist/jquery.min.js",
+                        "~/Content/Proyecto/build/js/custom.min.js",
+                        "~/Content/Proyecto/bootstrap/dist/js/bootstrap.min.js",
+                        "~/Content/Proyecto/vendors/fastclick/lib/fastclick.js",
+                        "~/Content/Proyecto/vendors/nprogress/nprogress.js",
+                        "~/Content/Proyecto/vendors/Chart.js/dist/Chart.min.js",
+                        "~/Content/Proyecto//gauge.js/dist/gauge.min.js",
+                        "~/Content/Proyecto/bootstrap-progressbar/bootstrap-progressbar.min.js",
+                        "~/Content/Proyecto/iCheck/icheck.min.js",
+                        "~/Content/Proyecto/skycons/skycons.js",
+                        "~/Content/Proyecto/Flot/jquery.flot.js",
+                        "~/Content/Proyecto/Flot/jquery.flot.pie.js",
+                        "~/Content/Proyecto/Flot/jquery.flot.time.js",
+                        "~/Content/Proyecto/Flot/jquery.flot.stack.js",
+                        "~/Content/Proyecto/Flot/jquery.flot.resize.js",
+                        "~/Content/Proyecto/flot.orderbars/js/jquery.flot.orderBars.js",
+                        "~/Content/Proyecto/flot-spline/js/jquery.flot.spline.min.js",
+                        "~/Content/Proyecto/flot.curvedlines/curvedLines.js",
+                        "~/Content/Proyecto/DateJS/build/date.js",
+                        "~/Content/Proyecto/jqvmap/dist/jquery.vmap.js",
+                        "~/Content/Proyecto/jqvmap/dist/maps/jquery.vmap.world.js",
+                        "~/Content/Proyecto/examples/js/jquery.vmap.sampledata.js",
+                        "~/Content/Proyecto/moment/min/moment.min.js",
+                        "~/Content/Proyecto/vendors/bootstrap-daterangepicker/daterangepicker.js"
+                        ));
+
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
@@ -29,6 +55,9 @@ namespace IdentitySample
             bundles.Add(new StyleBundle("~/Content/").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/loader-style.css"));
+
+            bundles.Add(new StyleBundle("~/view").Include(
+                      "~/Content/Loader-style2.css"));
 
         }
     }
